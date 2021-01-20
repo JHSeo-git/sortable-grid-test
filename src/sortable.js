e2f79e1fe3ba97924ca85907d9fd0a1f1cf1c972
JHSeo-git <qnfqnfqnf@gmail.com>
2021-01-20 09:50:37 +0900

    add style

--- src/sortable.js
+++ src/sortable.js
@@ -3,10 +3,24 @@ const gridDemo = document.getElementById('gridDemo');
 function sortableInit() {
   if (Sortable) {
     new Sortable(gridDemo, {
+      float: true,
+      swap: true,
+      swapThreshold: 0.3,
+      handle: '.handle',
       animation: 150,
       ghostClass: 'blue-background-class',
+      swapClass: 'swap-highlight-class',
+      forceFallback: true,
+      fallbackClass: 'sortable-fallback',
+      fallbackTolerance: 50,
+      onMove: function (evt, originalEvt) {
+        console.log(evt);
+        console.log(originalEvt);
+      },
     });
     console.log('>> Sortable.js init success');
+  } else {
+    console.log('>> Sortable.js init fail');
   }
 }
 
